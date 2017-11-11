90788d6dc9d03e95a2d3b52e117a8c8a885cb24d
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-11 22:23:59 +0300

    Revert "откат"
    This reverts commit 7f2d5b3eabb33c6b17df959b94dd94b7203b58ad.

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/SessionHelper.java
@@ -1,4 +1,20 @@
 package ru.staq.pft.addressbook.appmanager;
 
-public class SessionHelper {
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SessionHelper extends HelperBase {
+
+
+  public SessionHelper(FirefoxDriver wd)  {
+
+    super(wd);
+  }
+
+  public void login(String username, String password) {
+    type(By.name("user"),username);
+    type(By.name("pass"),password);
+    click(By.xpath("//form[@id='LoginForm']/input[3]"));
+
+  }
 }
