07ea464dd45f87a5e86994bcb4ae543ae209b461
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-12 13:35:38 +0300

    Создаю пакет tests

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/AddNewTests.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/AddNewTests.java
@@ -8,10 +8,10 @@ public class AddNewTests extends TestBase{
   @Test
   public void testAddNew() {
 
-    gotoAddNewPage();
-    fillAddNewPage(new AddNewData("Evgeniy", "Aleksandrovich", "Egorov", "egorzhekov", "DPD", "Mosqow", "8-968-982-38-07", "egorzhekov@gmail.com", "1989"));
-    submitAddNew();
-    returnToHomePage();
+    app.gotoAddNewPage();
+    app.fillAddNewPage(new AddNewData("Evgeniy", "Aleksandrovich", "Egorov", "egorzhekov", "DPD", "Mosqow", "8-968-982-38-07", "egorzhekov@gmail.com", "1989"));
+    app.submitAddNew();
+    app.returnToHomePage();
   }
 
 
--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/TestBase.java
@@ -1,132 +1,24 @@
 package ru.staq.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestBase {
-  FirefoxDriver wd;
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+  protected final ApplicationManager app = new ApplicationManager();
 
   @BeforeMethod
   public void setUp() throws Exception {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-
-  }
-
-  protected void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  protected void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  protected void fillGroupForm(GroupDate groupDate) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupDate.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
-  }
-
-  protected void initGroupCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[4]")).click();
-  }
-
-  protected void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    app.init();
   }
 
   @AfterMethod
-  public void tearDown() {
-    wd.quit();
+  public void tearDown()
+  {
+    app.stop();
   }
 
-  protected void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
-  }
-
-  protected void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
-  }
-
-  protected void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  protected void submitAddNew() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  protected void fillAddNewPage(AddNewData addNewData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(addNewData.getFirstname());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(addNewData.getMiddlename());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(addNewData.getLastname());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(addNewData.getNickname());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(addNewData.getCompany());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(addNewData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(addNewData.getMobile());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(addNewData.getEmail());
-    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).isSelected()) {
-      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).click();
-    }
-    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).isSelected()) {
-      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).click();
-    }
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(addNewData.getByear());
-  }
-
-  protected void gotoAddNewPage() {
-    wd.findElement(By.linkText("add new")).click();
+  public ApplicationManager getApp() {
+    return app;
   }
 }
