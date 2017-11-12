b5faa608db62542fcfd4c3cb8b38425d0d9f625a
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-12 13:11:42 +0300

    Revert "Revert "Revert "необходимо откатиться"""
    This reverts commit a4ba2e08874a1b961ea117c84f0dbd28e8015bfa.

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/AddNewHelper.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/AddNewHelper.java
@@ -4,30 +4,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.staq.pft.addressbook.model.AddNewData;
 
-public class AddNewHelper extends HelperBase{
-
+public class AddNewHelper {
+  private FirefoxDriver wd;
 
   public AddNewHelper(FirefoxDriver wd) {
-    super(wd);
+    this.wd = wd;
   }
 
   public void returnToHomePage() {
-    click(By.linkText("home page"));
+    wd.findElement(By.linkText("home page")).click();
   }
 
   public void submitAddNew() {
-    click(By.xpath("//div[@id='content']/form/input[21]"));
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
   public void fillAddNewPage(AddNewData addNewData) {
-    type(By.name("firstname"),addNewData.getFirstname());
-    type(By.name("middlename"),addNewData.getMiddlename());
-    type(By.name("lastname"),addNewData.getLastname());
-    type(By.name("nickname"),addNewData.getNickname());
-    type(By.name("company"),addNewData.getCompany());
-    type(By.name("address"),addNewData.getAddress());
-    type(By.name("mobile"),addNewData.getMobile());
-    
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(addNewData.getFirstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(addNewData.getMiddlename());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(addNewData.getLastname());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(addNewData.getNickname());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(addNewData.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(addNewData.getAddress());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(addNewData.getMobile());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
     wd.findElement(By.name("email")).sendKeys(addNewData.getEmail());
--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/GroupHelper.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.staq.pft.addressbook.model.GroupDate;
 
-public class GroupHelper extends HelperBase {
+public class GroupHelper {
  private FirefoxDriver wd;
 
   public GroupHelper(FirefoxDriver wd) {
@@ -12,38 +12,34 @@ public class GroupHelper extends HelperBase {
   }
 
   public void returnToGroupPage() {
-    click(By.linkText("group page"));
+    wd.findElement(By.linkText("group page")).click();
   }
 
   public void submitGroupCreation() {
-    click(By.name("submit"));
-  }
-
-  private void click(By locator) {
-    wd.findElement(locator).click();
+    wd.findElement(By.name("submit")).click();
   }
 
   public void fillGroupForm(GroupDate groupDate) {
-    type(By.name("group_name"), groupDate.getName());
-    type(By.name("group_header"), groupDate.getHeader());
-    type(By.name("group_footer"), groupDate.getFooter());
-  }
-
-  private void type(By locator, String text) {
-    click(locator);
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupDate.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
   }
 
   public void initGroupCreation() {
-    click(By.xpath("//div[@id='content']/form/input[4]"));
+    wd.findElement(By.xpath("//div[@id='content']/form/input[4]")).click();
   }
 
   public void deleteSelectedGroups() {
-    click(By.name("delete"));
+      wd.findElement(By.name("delete")).click();
   }
 
   public void selectGroup() {
-    click(By.name("selected[]"));
+      wd.findElement(By.name("selected[]")).click();
   }
 }
