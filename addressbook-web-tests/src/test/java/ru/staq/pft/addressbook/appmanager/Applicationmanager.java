193abd7cb31c44d658a0b0d774c735fc41d42145
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-12 13:20:17 +0300

    Revert "Создал вспомогательный класс AddNewHelper. Думаю куда деть gotoHomePage"
    This reverts commit fbb67886a2c344b4fd5b3e6098403b48aaae3be3.

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/Applicationmanager.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/Applicationmanager.java
@@ -4,16 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import ru.staq.pft.addressbook.model.AddNewData;
+import ru.staq.pft.addressbook.model.GroupDate;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager {
-
+public class Applicationmanager {
   FirefoxDriver wd;
-
-  private  AddNewHelper addNewHelper;
-  private NavigationHelper navigationHelper;
-  private  GroupHelper groupHelper;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
     try {
@@ -28,9 +25,7 @@ public class ApplicationManager {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    addNewHelper = new AddNewHelper(wd);
+
     login("admin", "secret");
   }
 
@@ -45,23 +40,91 @@ public class ApplicationManager {
 
   }
 
+  public void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  public void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  public void fillGroupForm(GroupDate groupDate) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupDate.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
+  }
+
+  public void initGroupCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[4]")).click();
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
   public void stop() {
     wd.quit();
   }
 
+  public void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+  }
+
+  public void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+  }
+
+  public void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  public void submitAddNew() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  public void fillAddNewPage(AddNewData addNewData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(addNewData.getFirstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(addNewData.getMiddlename());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(addNewData.getLastname());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(addNewData.getNickname());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(addNewData.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(addNewData.getAddress());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(addNewData.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(addNewData.getEmail());
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).click();
+    }
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).click();
+    }
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(addNewData.getByear());
+  }
+
   public void gotoAddNewPage() {
     wd.findElement(By.linkText("add new")).click();
-  }
-
-  public GroupHelper getGroupHelper() {
-    return groupHelper;
-  }
-
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
-  }
-
-  public AddNewHelper getAddNewHelper() {
-    return addNewHelper;
   }
 }
