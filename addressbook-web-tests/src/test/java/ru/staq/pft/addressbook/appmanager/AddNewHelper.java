589b6192a2cd880a48d3da069facfffa708ad0de
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-12 14:27:24 +0300

    Задание 5.

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/AddNewHelper.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/AddNewHelper.java
@@ -4,54 +4,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.staq.pft.addressbook.model.AddNewData;
 
-public class AddNewHelper {
-  private FirefoxDriver wd;
+public class AddNewHelper extends  HelperBase{
+
 
   public AddNewHelper(FirefoxDriver wd) {
-    this.wd=wd;
+   super(wd);
   }
 
   public void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
+    click(By.linkText("home page"));
   }
 
   public void submitAddNew() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillAddNewPage(AddNewData addNewData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(addNewData.getFirstname());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(addNewData.getMiddlename());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(addNewData.getLastname());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(addNewData.getNickname());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(addNewData.getCompany());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(addNewData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(addNewData.getMobile());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(addNewData.getEmail());
+    type(By.name("firstname"),addNewData.getFirstname());
+    type(By.name("middlename"),addNewData.getMiddlename());
+    type(By.name("lastname"),addNewData.getLastname());
+    type(By.name("nickname"),addNewData.getNickname());
+    type(By.name("company"),addNewData.getCompany());
+    type(By.name("address"),addNewData.getAddress());
+    type(By.name("mobile"),addNewData.getMobile());
+    type(By.name("email"),addNewData.getEmail());
+
     if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).isSelected()) {
-      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).click();
+      click(By.xpath("//div[@id='content']/form/select[1]//option[16]"));
     }
     if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).isSelected()) {
-      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).click();
+      click(By.xpath("//div[@id='content']/form/select[2]//option[3]"));
     }
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(addNewData.getByear());
+    type(By.name("byear"),addNewData.getByear());
+
   }
 }
--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.staq.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -17,14 +16,7 @@ public class ApplicationManager {
   private  AddNewHelper addNewHelper;
   private  GroupHelper groupHelper;
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+
 
   public void init() {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
