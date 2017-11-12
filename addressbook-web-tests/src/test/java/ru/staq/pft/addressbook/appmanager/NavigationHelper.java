01b0581e297cc83ba9d01a5812d12ee2c89fa769
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-12 12:54:38 +0300

    Revert "Revert "Перенес gotoAddNewPage в NavigationHelper. В предыдущем коммите не gotoHomePage a gotoAddNewpage""
    This reverts commit 3dbc1f96d841649a3c67acb40be2953463f66069.

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,14 +3,9 @@ package ru.staq.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-<<<<<<< HEAD
 public class NavigationHelper extends HelperBase{
 
 
-=======
-public class NavigationHelper {
- private FirefoxDriver wd;
->>>>>>> parent of 8412d75... Перенес gotoAddNewPage в NavigationHelper. В предыдущем коммите не gotoHomePage a gotoAddNewpage
 
   public NavigationHelper(FirefoxDriver wd) {
     super(wd);
@@ -19,11 +14,8 @@ public class NavigationHelper {
   public void gotoGroupPage() {
     click(By.linkText("groups"));
   }
-<<<<<<< HEAD
 
   public void gotoAddNewPage() {
     click(By.linkText("add new"));
   }
-=======
->>>>>>> parent of 8412d75... Перенес gotoAddNewPage в NavigationHelper. В предыдущем коммите не gotoHomePage a gotoAddNewpage
 }
