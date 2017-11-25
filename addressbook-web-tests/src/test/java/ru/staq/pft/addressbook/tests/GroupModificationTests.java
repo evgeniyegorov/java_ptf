cf2cfe1a0b8348b5b7c33af77f63a50d8e47ffdc
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-11-25 17:33:30 +0300

    Лекция 4.6. Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.staq.pft.addressbook.model.GroupDate;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
@@ -11,17 +13,17 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification()
   {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupDate> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup( before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupDate> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
     
   }
 }
