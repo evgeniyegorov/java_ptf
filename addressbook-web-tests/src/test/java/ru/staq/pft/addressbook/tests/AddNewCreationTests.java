0a0c7847dbafe29e4eb8538fe347bab8a6d528d7
evgeniyegorov <33195508+evgeniyegorov@users.noreply.github.com>
2017-12-05 22:05:02 +0300

    6.4 для контактов

--- addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/tests/AddNewCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/staq/pft/addressbook/tests/AddNewCreationTests.java
@@ -17,28 +17,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AddNewCreationTests extends TestBase{
 
-  /*@DataProvider
+  @DataProvider
   public Iterator<Object[]> validAddNews() {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new GroupDate().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[] {new GroupDate().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[] {new GroupDate().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    list.add(new Object[] {new AddNewData().withFirstname("Evgeniy").withMiddlename("Aleksandrovich")
+            .withLastname("Egorov").withNickname("egorzhekov").withCompany("DPD").withAddress("Mosqow")
+            .withMobilePhone("8-968-982-38-07").withEmail("egorzhekov@gmail.com").withByear("1989")
+            .withGroup("test1")});
+    list.add(new Object[] {new AddNewData().withFirstname("Evgeniy").withMiddlename("Aleksandrovich")
+            .withLastname("Egorov").withNickname("egorzhekov").withCompany("DPD").withAddress("Mosqow")
+            .withMobilePhone("8-968-982-38-07").withEmail("egorzhekov@gmail.com").withByear("1989")
+            .withGroup("test1")});
+    list.add(new Object[] {new AddNewData().withFirstname("Evgeniy").withMiddlename("Aleksandrovich")
+            .withLastname("Egorov").withNickname("egorzhekov").withCompany("DPD").withAddress("Mosqow")
+            .withMobilePhone("8-968-982-38-07").withEmail("egorzhekov@gmail.com").withByear("1989")
+            .withGroup("test1")});
     return list.iterator();
-  }*/
+  }
 
 
-  @Test
-  public void testAddNew() {
-
+  @Test(dataProvider = "validAddNews")
+  public void testAddNew(AddNewData addnew) {
     AddNews before = app.addNew().all();
     app.goTo().addNewPage();
     File photo = new File("src/test/resources/stru.png");
-
-    AddNewData addnew = new AddNewData().withFirstname("Evgeniy").withMiddlename("Aleksandrovich")
-            .withLastname("Egorov").withNickname("egorzhekov").withCompany("DPD").withAddress("Mosqow")
-            .withMobilePhone("8-968-982-38-07").withEmail("egorzhekov@gmail.com").withByear("1989")
-            .withGroup("test1").withPhoto(photo);
-
     app.addNew().create(addnew, true);
     assertThat(app.addNew().count(), equalTo(before.size() + 1));
     AddNews after = app.addNew().all();
